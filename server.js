7ca063b82ccf42dc506ba9ecdc54340a49d79f58
kyoungd <kyoungd@hotmail.com>
2021-11-06 16:36:46 -0700

    clean up

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: { origins: '*:*' }
 });
+const moment = require('moment');
 
 const formatMessage = require("./utils/messages");
 const _ = require('underscore');
@@ -27,6 +28,10 @@ const client = redis.createClient();
 app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "ChatCord Bot";
+const SCOREKEY = process.env.SCOREKEY || 'STUDYTHREEBARSCORE';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const INTERVAL_MS = process.env.INTERVAL_MS || 5000
 
 // run when a client connects
 io.on("connection", (socket) => {
@@ -78,7 +83,31 @@ io.on("connection", (socket) => {
   });
 });
 
-const SCOREKEY = process.env.SCOREKEY || 'STUDYTHREEBARSCORE';
+
+const getScores = (data) => {
+  let scores = [];
+  for (let item in data) {
+    try {
+      const block = JSON.parse(data[item]);
+      for (let ix = 0; ix < block.length; ++ix) {
+        const dataItem = { ...block[ix], Score: block[ix].point };
+        scores.push(dataItem);
+      }
+      // studyIndicators = (item in stack) ? stack[item] : {};
+      // const block = JSON.parse(data[item]);
+      // const dataItem = { ...block, StudyMark: JSON.parse(studyIndicators).value };
+      // scores.push(dataItem);
+    }
+    catch (e) {
+      console.log('error: ', e);
+    }
+  }
+  // sort scores by Score in descending order
+  scores = _.sortBy(scores, 'Score').reverse();
+  msg = JSON.stringify(scores);
+  console.log(room);
+  return scores;
+}
 
 const interval = setInterval(() => {
   const allRooms = getAllUniqueRooms();
@@ -90,10 +119,20 @@ const interval = setInterval(() => {
         // convert data to array
         let scores = [];
         for (let item in data) {
-          studyIndicators = (item in stack) ? stack[item] : {};
-          const block = JSON.parse(data[item]);
-          const dataItem = { ...block, StudyMark: JSON.parse(studyIndicators).value };
-          scores.push(dataItem);
+          try {
+            const block = JSON.parse(data[item]);
+            for (let ix = 0; ix < block.length; ++ix) {
+              const dataItem = { ...block[ix], Score: block[ix].point };
+              scores.push(dataItem);
+            }
+            // studyIndicators = (item in stack) ? stack[item] : {};
+            // const block = JSON.parse(data[item]);
+            // const dataItem = { ...block, StudyMark: JSON.parse(studyIndicators).value };
+            // scores.push(dataItem);
+          }
+          catch (e) {
+            console.log('error: ', e);
+          }
         }
         // sort scores by Score in descending order
         scores = _.sortBy(scores, 'Score').reverse();
@@ -104,11 +143,42 @@ const interval = setInterval(() => {
       });
     });
   });
-}, 5000);
+}, INTERVAL_MS);
 
-const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+const cleanDatetime = (data) => {
+  const datablock = []
+  for (let ix = 0; ix < data.length; ++ix) {
+    block = data[ix]
+    eventdate = moment.unix(block.t)
+    block['date'] = eventdate.format('h:mm:ss a')
+    block['seconds'] = moment().diff(eventdate, 'seconds')
+    datablock.push(block)
+  }
+  return datablock
+}
+
+app.get('/data', function (req, res) {
+  room = 'STUDYTHREEBARSCORE';
+  results = [];
+  client.hgetall(room, function (err, data) {
+    scores = getScores(data);
+    for (let ix = 0; ix < scores.length; ++ix) {
+      score = scores[ix]
+      // load timestamp into moment object
+      // eventdate = moment.unix(score.timestamp)
+      // score['date'] = eventdate.format('MMMM Do YYYY, h:mm:ss a')
+      // score['seconds'] = moment().diff(eventdate, 'seconds')
+      score['data'] = cleanDatetime(score.data)
+      results.push(score);
+    }
+    results = results.filter(item => item.point > 0)
+    results = _.sortBy(results, 'timestamp').reverse();
+    res.send(results);
+  });
+})
+
+server.listen(PORT, HOST, () => console.log(`Server running on port: ${PORT}`));
 
 // kill server port
 // kill -9 $(lsof -t -i:3001)
